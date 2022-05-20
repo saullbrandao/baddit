894e6e0847a57671d29b59f7de5d17702b7049b9
Saull Brandão <saull@outlook.com>
2022-05-20 10:45:05 -0300

    update the seeds file with user authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,35 @@
 require 'faker'
 
-['rails', 'ruby', 'webdev', 'fullstack', 'programming'].each do |name|
-  Community.create(name: name)
+# Create users
+30.times do |i|
+  User.create(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: 'password',
+    password_confirmation: 'password'
+  )
 end
 
-Community.all.each do |community|
-  (1..3).each do |i|
-    community.posts.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph_by_chars(number: 500), upvotes: rand(0..50), downvotes: rand(0..50))
-    community.posts.last.comments.create(body: Faker::Lorem.sentence)
+users = User.all
+
+# Create communities
+['rails', 'ruby', 'webdev', 'fullstack', 'programming'].each do |name|
+  Community.create(name: name, owner_id: users.sample.id )
+end
+
+communities = Community.all
+
+# Create posts and comments
+communities.each do |community|
+  3.times do |i|
+    community.posts.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph_by_chars(number: 500), upvotes: rand(0..50), downvotes: rand(0..50), user: users.sample)
+    community.posts.last.comments.create(body: Faker::Lorem.sentence, user: users.sample)
   end
 end
+
+User.create(
+  email: 'test@test',
+  username: 'test',
+  password: '123123',
+  password_confirmation: '123123'
+)
