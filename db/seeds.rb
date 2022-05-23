a1d29cfc20e7511e3c0dfe0a5dc5e1ebe7d547ca
Saull Brandão <saull@outlook.com>
2022-05-23 10:55:36 -0300

    update the seeds file to create posts and comments with different dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,10 +22,14 @@ communities = Community.all
 # Create posts and comments
 communities.each do |community|
   3.times do |i|
-    post = community.posts.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph_by_chars(number: 500), user: users.sample)
+    post = community.posts
+                    .create!(title: Faker::Lorem.sentence,
+                             body: Faker::Lorem.paragraph_by_chars(number: 500), 
+                             user: users.sample, 
+                             created_at: Faker::Time.between(from: 3.month.ago, to: Time.now))
     
     rand(1..30).times do |i|
-      post.comments.create!(body: Faker::Lorem.sentence, user: users.sample)
+      post.comments.create!(body: Faker::Lorem.sentence, user: users.sample, created_at: Faker::Time.between(from: post.created_at, to: Time.now))
       post.votes.create!(user: users[i], vote: [1, -1].sample)
     end
   end
