5536e3dcf32fb67b5a22a4d0f8d1b0e9d575d329
Saull Brandão <saull@outlook.com>
2022-05-26 08:07:44 -0300

    update seeds file to create votes on comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,9 @@ users = User.all
   Community.create!(name: name, owner_id: users.sample.id )
 end
 
+# Create posts and comments
 communities = Community.all
 
-# Create posts and comments
 communities.each do |community|
   3.times do |i|
     post = community.posts
@@ -28,13 +28,20 @@ communities.each do |community|
                              user: users.sample, 
                              created_at: Faker::Time.between(from: 3.month.ago, to: Time.now))
     
-    rand(1..30).times do |i|
+    rand(0..20).times do |i|
       post.comments.create!(body: Faker::Lorem.sentence, user: users.sample, created_at: Faker::Time.between(from: post.created_at, to: Time.now))
       post.votes.create!(user: users[i], vote: [1, -1].sample)
     end
   end
 end
 
+Comment.all.each do |comment|
+  rand(0..20).times do |i|
+    comment.votes.create!(user: users[i], vote: [1, -1].sample)
+  end
+end
+
+# Test user
 User.create!(
   email: 'test@test',
   username: 'test',
